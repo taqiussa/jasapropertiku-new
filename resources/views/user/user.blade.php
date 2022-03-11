a013d36ba9eda848cee7699d6ef1fb1cbd214d2d
taqiussa <taqiussa@gmail.com>
2022-03-11 07:30:55 +0700

    user  auth() to facade

--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -7,66 +7,66 @@
                 <span>
                     User Profile 
                 </span>
-                <a href="{{ route('user.edit', auth()->user()->slug) }}" class="btn btn-success" role="button">
+                <a href="{{ route('user.edit', Auth::user()->name->slug) }}" class="btn btn-success" role="button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
                     Edit Profile
                 </a>
             </div>
             <div class="card-body">
                 <div class="img my-2">
-                    <img src="{{ asset('storage/'.auth()->user()->photo) }}" alt="" class="img-thumbnail rounded-circle">
+                    <img src="{{ asset('storage/'.Auth::user()->name->photo) }}" alt="" class="img-thumbnail rounded-circle">
                 </div>
                 <div class="form-floating mb-3">
-                    <input disabled value="{{ auth()->user()->name }}" type="text" class="form-control" id="floatingNama" placeholder="Nama Lengkap..."
+                    <input disabled value="{{ Auth::user()->name->name }}" type="text" class="form-control" id="floatingNama" placeholder="Nama Lengkap..."
                         autocomplete="off">
                     <label for="floatingNama">Nama Lengkap</label>
                 </div>
                 <div class="form-floating mb-3">
-                    <input disabled value="{{ auth()->user()->birthplace }}" type="text" class="form-control" id="floatingTempatLahir" placeholder="Tempat Lahir..."
+                    <input disabled value="{{ Auth::user()->name->birthplace }}" type="text" class="form-control" id="floatingTempatLahir" placeholder="Tempat Lahir..."
                         autocomplete="off">
                     <label for="floatingTempatLahir">Tempat Lahir</label>
                 </div>
                 <div class="form-floating mb-3">
-                    <input disabled value="{{ auth()->user()->birthdate }}" type="date" class="form-control" id="floatingTanggalLahir"
+                    <input disabled value="{{ Auth::user()->name->birthdate }}" type="date" class="form-control" id="floatingTanggalLahir"
                         autocomplete="off">
                     <label for="floatingTanggalLahir">Tanggal Lahir</label>
                 </div>
                 <div class="form-floating mb-3">
-                    <textarea disabled class="form-control" placeholder="Alamat Lengkap" id="floatingAlamat">{{ auth()->user()->address }}</textarea>
+                    <textarea disabled class="form-control" placeholder="Alamat Lengkap" id="floatingAlamat">{{ Auth::user()->name->address }}</textarea>
                     <label for="floatingAlamat">Alamat Lengkap</label>
                 </div>
                 <div class="form-floating mb-3">
                     <select disabled class="form-select" id="floatingProvinsi" aria-label="Floating label select example">
-                        <option value="" selected>{{ auth()->user()->indonesiaProvince->name }}</option>
+                        <option value="" selected>{{ Auth::user()->name->indonesiaProvince->name }}</option>
                     </select>
                     <label for="floatingProvinsi">Provinsi</label>
                 </div>
                 <div class="form-floating mb-3">
                     <select disabled class="form-select" id="floatingKota" aria-label="Floating label select example">
-                        <option value="" selected>{{ auth()->user()->indonesiaCity->name }}</option>
+                        <option value="" selected>{{ Auth::user()->name->indonesiaCity->name }}</option>
                     </select>
                     <label for="floatingKota">Kota</label>
                 </div>
                 <div class="form-floating mb-3">
                     <select disabled class="form-select" id="floatingKecamatan" aria-label="Floating label select example">
-                        <option value="" selected>{{ auth()->user()->indonesiaDistrict->name }}</option>
+                        <option value="" selected>{{ Auth::user()->name->indonesiaDistrict->name }}</option>
                     </select>
                     <label for="floatingKecamatan">Kecamatan</label>
                 </div>
                 <div class="form-floating mb-3">
                     <select disabled class="form-select" id="floatingDesa" aria-label="Floating label select example">
-                        <option value="" selected>{{ auth()->user()->indonesiaVillage->name }}</option>
+                        <option value="" selected>{{ Auth::user()->name->indonesiaVillage->name }}</option>
                     </select>
                     <label for="floatingDesa">Desa</label>
                 </div>
                 <div class="form-floating mb-3">
                     <select disabled class="form-select" id="floatingJenisKelamin" aria-label="Floating label select example">
-                        <option selected>{{ auth()->user()->gender }}</option>
+                        <option selected>{{ Auth::user()->name->gender }}</option>
                     </select>
                     <label for="floatingJenisKelamin">Jenis Kelamin</label>
                 </div>
                 <div class="form-floating mb-3">
-                    <input disabled value="{{ auth()->user()->whatsapp }}" type="text" class="form-control" id="floatingNoWhatsApp" placeholder="No. WhatsApp">
+                    <input disabled value="{{ Auth::user()->name->whatsapp }}" type="text" class="form-control" id="floatingNoWhatsApp" placeholder="No. WhatsApp">
                     <label for="floatingNoWhatsApp">No. WhatsApp</label>
                 </div>
             </div>
