4ee299ca8e167e623035d6db5a3366038caccb27
taqiussa <taqiussa@gmail.com>
2022-03-11 07:39:45 +0700

    a

--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.guest')
+@extends('layouts.app')
 @section('content')
     <div class="min-vh-100 my-5 d-flex justify-content-center row px-2">
         @include('partials.user.user-submenu')
