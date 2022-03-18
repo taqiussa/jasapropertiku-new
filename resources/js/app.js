4031081534a783cdbc8ee88fbba0f228104f09bb
taqiussa <taqiussa@gmail.com>
2022-03-18 19:12:28 +0700

    a

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,35 +1,35 @@
 require('./bootstrap');
-require('@fortawesome/fontawesome-free');
-const feather = require('feather-icons');
-feather.replace();
-import { Notyf } from 'notyf';
-import swal from 'sweetalert';
-window.addEventListener('swal:notif', event => {
-    swal({
-        title: event.detail.title,
-        text: event.detail.text,
-        icon: event.detail.type,
-    });
-});
-var notyf = new Notyf({
-    duration: 5000,
-    position: {
-        x: 'right',
-        y: 'top',
-    },
-    dismissible: true,
-    types: [
-        {
-            type: 'warning',
-            background: 'orange',
-            icon: false,
-        },
-    ]
-});
-window.addEventListener('notyf:success', event => {
-    // notyf[event.detail.type](event.detail.message);
-    notyf.open({
-        type: event.detail.type,
-        message: event.detail.message,
-    });
-});
+// require('@fortawesome/fontawesome-free');
+// const feather = require('feather-icons');
+// feather.replace();
+// import { Notyf } from 'notyf';
+// import swal from 'sweetalert';
+// window.addEventListener('swal:notif', event => {
+//     swal({
+//         title: event.detail.title,
+//         text: event.detail.text,
+//         icon: event.detail.type,
+//     });
+// });
+// var notyf = new Notyf({
+//     duration: 5000,
+//     position: {
+//         x: 'right',
+//         y: 'top',
+//     },
+//     dismissible: true,
+//     types: [
+//         {
+//             type: 'warning',
+//             background: 'orange',
+//             icon: false,
+//         },
+//     ]
+// });
+// window.addEventListener('notyf:success', event => {
+//     // notyf[event.detail.type](event.detail.message);
+//     notyf.open({
+//         type: event.detail.type,
+//         message: event.detail.message,
+//     });
+// });
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,10 +9,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    {{-- icon --}}
     <link rel="icon" href="{{ asset('images/jameson.svg') }}" type="image/svg" sizes="16x16">
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,9 +18,11 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('fontawesome/css/all.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('notyf/notyf.min.css') }}" rel="stylesheet" />
     @stack('styles')
-    @stack('scripts')
 </head>
+
 <body>
     <div id="app">
         @include('partials.navbar')
@@ -32,9 +33,18 @@
         @endif
         <main class="container py-5 min-vh-100">
             @yield('content')
-            {{ $slot ?? '' }}
         </main>
         @include('partials.footer')
     </div>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/script.js') }}" defer></script>
+    <script src="{{ asset('notyf/notyf.min.js') }}"></script>
+    <script src="{{ asset('sweetalert/sweetalert.min.js') }}"></script>
+    <script src="{{ asset('fontawesome/js/fontawesome.min.js') }}"></script>
+    <script src="{{ asset('feather-icons/dist/feather.min.js') }}"></script>
+    @stack('scripts')
+    
 </body>
+
 </html>
